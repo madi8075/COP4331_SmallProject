38a275f58b0b0beab733c26543e731770991b8a7
root <kristianjeanmichel@gmail.com>
2023-09-22 21:09:13 +0000

    hosted on lamp and made files into php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Starfinder</title>
+</head>
+<body>
+    <div>Hello, Poopy Pants</div>
+</body>
+</html>
