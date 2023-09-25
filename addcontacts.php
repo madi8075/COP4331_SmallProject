520aec1cabc388d734c02e5fa4388bb36332992a
root <kristianjeanmichel@gmail.com>
2023-09-25 14:20:45 +0000

    added search

--- addcontacts.php
+++ addcontacts.php
@@ -1,17 +1,21 @@
 <?php
-    ob_start();  // Start output buffering at the beginning
+    ob_start();  // Starts output buffering to prevent unwanted output before headers.
 
+    // Check if the request method is POST.
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // Include the database connection file
+
+        // Connect to the database.
         require 'connection.php';
         
-        // Start the session
+        // Start a session.
         session_start();
         
+        // Set the content type of the response to JSON.
         header('Content-Type: application/json');
 
-        $response = array();
+        $response = array(); // Array to store response data.
 
+        // Check if the user is logged in.
         if (!isset($_SESSION['user_id'])) {
             $response["status"] = "error";
             $response["message"] = "User not logged in";
@@ -19,14 +23,17 @@
             exit;
         }
 
+        // Get the JSON payload from the request.
         $data = json_decode(file_get_contents('php://input'), true);
 
+        // Check if necessary data exists in the payload.
         if (isset($data['name']) && isset($data['email']) && isset($data['phone'])) {
             $user_id = $_SESSION['user_id'];
             $name = $data['name'];
             $email = $data['email'];
             $phone = $data['phone'];
 
+            // Query to check if email is already associated with this user.
             $checkEmailAndUserID = "SELECT * FROM contacts WHERE email = ? AND userID = ?";
             $stmt = $conn->prepare($checkEmailAndUserID);
             $stmt->bind_param("ss", $email, $user_id);
@@ -35,9 +42,11 @@
             $stmt->close();
 
             if ($result->num_rows > 0) {
+                // If the email is already associated, return an error.
                 $response["status"] = "error";
                 $response["message"] = "Email already associated with this user!";
             } else {
+                // Otherwise, insert the new contact into the database.
                 $stmt = $conn->prepare("INSERT INTO contacts (username, email, phone, userID) VALUES (?, ?, ?, ?)");
                 $stmt->bind_param("ssss", $name, $email, $phone, $user_id);
                 $stmt->execute();
@@ -45,12 +54,13 @@
                 $response["status"] = "success";
                 $response["message"] = "Contact added successfully!";
             }
+            // Echo the response as JSON and exit the script.
             echo json_encode($response);
-            exit; // Make sure to exit after sending the response.
+            exit;
         }
     }
 
-    ob_end_clean();  // End output buffering and discard any captured output
+    ob_end_clean();  // End output buffering and discard any captured output.
 ?>
 
 <!DOCTYPE html>
@@ -60,14 +70,18 @@
     <meta charset="UTF-8">
     <title>Add Contact</title>
     <script>
+        // JavaScript that runs when the document is loaded.
         document.addEventListener("DOMContentLoaded", function() {
+            // Attach an event listener to the form submission.
             document.getElementById("addContactForm").addEventListener("submit", async function(e) {
                 e.preventDefault();
 
+                // Retrieve form data.
                 let name = document.getElementById("name").value;
                 let email = document.getElementById("email").value;
                 let phone = document.getElementById("phone").value;
 
+                // Make an asynchronous POST request to add the contact.
                 let response = await fetch('addcontacts.php', {
                     method: 'POST',
                     headers: {
@@ -78,9 +92,12 @@
 
                 let data = await response.json();
 
+                // Handle the response from the server.
                 if (data.status === 'success') {
+                    // If successful, redirect to the contacts page.
                     window.location.href = 'contacts.php';
                 } else {
+                    // If there's an error, display it.
                     document.getElementById("error_message").innerText = data.message;
                 }
             });
@@ -90,10 +107,10 @@
 
 <body>
 
-    <!-- Display the error message container -->
+    <!-- Display any error messages here. -->
     <p id="error_message" style="color:red;"></p>
 
-    <!-- Contact addition form -->
+    <!-- Form to add a contact. -->
     <form id="addContactForm">
         Name: <input type="text" id="name" required><br>
         Email: <input type="email" id="email" required><br>
@@ -101,6 +118,7 @@
         <input type="submit" value="Add Contact">
     </form>
 
+    <!-- Link to navigate back to the contacts page. -->
     <a href="contacts.php" class="button">Back to Contacts</a>
 
 </body>
--- contacts.php
+++ contacts.php
@@ -1,49 +1,57 @@
 <?php
-    // Include the database connection file
-    require 'connection.php';
+  // Include the database connection file
+require 'connection.php';
 
-    // Start the session
-    session_start();
+// Start the session
+session_start();
 
-    // Check for sign out request
-    if (isset($_GET['signout']) && $_GET['signout'] == 'true') {
-        session_destroy(); // Destroy the session
-        header('Location: index.php'); // Redirect to index.php
-        exit; // Stop further execution
-    }
+// Check for sign out request
+if (isset($_GET['signout']) && $_GET['signout'] == 'true') {
+    session_destroy(); // Destroy the session
+    header('Location: index.php'); // Redirect to index.php
+    exit; // Stop further execution
+}
 
-    if (isset($_POST['id'])) {
-        $contactId = $_POST['id'];
-        
-        // Use a prepared statement to avoid SQL injection
-        $stmt = $conn->prepare("DELETE FROM contacts WHERE id = ?");
-        $stmt->bind_param("i", $contactId);
-        
-        $success = $stmt->execute();
-        
-        header('Content-Type: application/json');
-        echo json_encode(['success' => $success]);
-        exit;
-    }
+// Handle deletion
+if (isset($_POST['id'])) {
+    $contactId = $_POST['id'];
+    
+    // Use a prepared statement to avoid SQL injection
+    $stmt = $conn->prepare("DELETE FROM contacts WHERE id = ?");
+    $stmt->bind_param("i", $contactId);
+    
+    $success = $stmt->execute();
+    
+    header('Content-Type: application/json');
+    echo json_encode(['success' => $success]);
+    exit;
+}
 
-    $contacts = []; // Empty array to store contacts
+$contacts = []; // Empty array to store contacts
 
     // If a user is logged in
     if (isset($_SESSION['user_id'])) {
-        $user_id = $_SESSION['user_id'];
+    $user_id = $_SESSION['user_id'];
+    $query = "SELECT * FROM contacts WHERE userID = '$user_id'";
 
-        // Fetch contacts of the user
-        $result = $conn->query("SELECT * FROM contacts WHERE userID = '$user_id'");
-
-        while ($row = $result->fetch_assoc()) {
-            $contacts[] = [
-                'id' => $row['id'],
-                'name' => $row['username'],
-                'email' => $row['email'],
-                'phone' => $row['phone']
-            ];
-        }
+    // If there is a search term in the GET request
+    if (isset($_GET['search']) && !empty($_GET['search'])) {
+        $searchTerm = $conn->real_escape_string($_GET['search']); // Secure the search term
+        $query .= " AND username LIKE '%$searchTerm%'";
     }
+
+    // Fetch contacts of the user based on the query
+    $result = $conn->query($query);
+
+    while ($row = $result->fetch_assoc()) {
+        $contacts[] = [
+            'id' => $row['id'],
+            'name' => $row['username'],
+            'email' => $row['email'],
+            'phone' => $row['phone']
+        ];
+    }
+}
 
     // If it's an AJAX request, return the JSON
     if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
@@ -54,104 +62,121 @@
 ?>
 
 <script>
-    // Fetch the contacts using AJAX
-    fetch('contacts.php', {
-        headers: {
-            "X-Requested-With": "XMLHttpRequest" // This header tells PHP it's an AJAX request
+document.addEventListener('DOMContentLoaded', function() {
+    // This function fetches the contacts based on the current search value
+    function fetchContacts() {
+        // Get the current value from the search input
+        let searchValue = document.getElementById('searchInput').value;
+        let fetchUrl = 'contacts.php';
+        
+        // If there's a search value, append it to the fetch URL
+        if (searchValue) {
+            fetchUrl += '?search=' + encodeURIComponent(searchValue);
         }
-    })
-    .then(response => response.json())
-    .then(contacts => {
-        // Get the table body
-        let tbody = document.querySelector('.contact-table tbody');
         
-        // Loop through the contacts and append to the table
-        contacts.forEach(contact => {
-    let tr = document.createElement('tr');
-    tr.innerHTML = `
-        <tr>
-        <td>${contact.name}</td>
-        <td>${contact.email}</td>
-        <td>${contact.phone}</td>
-        <td><button class="viewButton"><img src="images/magGlass.png" class="magGlass"></button></td>
-        <td><button class="viewButton"><img src="images/pencil1.png" class="magGlass"></button></td>
-        <td><button class="deleteButton" data-id="${contact.id}"><img src="images/trash.gif" class="magGlass"></button></td>
-        </tr>
-    `;
-    tbody.appendChild(tr);
-});
-
-    });
-
-    document.addEventListener('click', function(event) {
-    if (event.target.closest('.deleteButton')) {
-        const contactId = event.target.closest('.deleteButton').getAttribute('data-id');
-        
-        fetch('contacts.php', {
-            method: 'POST',
+        // Make an AJAX request to fetch contacts
+        fetch(fetchUrl, {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: 'id=' + contactId
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                // Remove the row from the table
-                event.target.closest('tr').remove();
-                alert('Contact deleted successfully!');
-            } else {
-                alert('Failed to delete the contact.');
+                "X-Requested-With": "XMLHttpRequest" // This header tells PHP it's an AJAX request
             }
+        })
+        .then(response => response.json()) // Convert the response to JSON
+        .then(contacts => {
+            // Clear the current table contents
+            let tbody = document.querySelector('.contact-table tbody');
+            tbody.innerHTML = '';
+            
+            // Loop through the returned contacts and add them to the table
+            contacts.forEach(contact => {
+                let tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <td>${contact.name}</td>
+                    <td>${contact.email}</td>
+                    <td>${contact.phone}</td>
+                    <td><button class="viewButton"><img src="images/magGlass.png" class="magGlass"></button></td>
+                    <td><button class="viewButton"><img src="images/pencil1.png" class="magGlass"></button></td>
+                    <td><button class="deleteButton" data-id="${contact.id}"><img src="images/trash.gif" class="magGlass"></button></td>
+                `;
+                tbody.appendChild(tr);
+            });
         });
     }
-});
 
+    // Prevent the search form from submitting and reloading the page, fetch contacts instead
+    document.getElementById('searchForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        fetchContacts();
+    });
+
+    // Whenever the search input value changes, fetch the contacts
+    document.getElementById('searchInput').addEventListener('input', function() {
+        fetchContacts();
+    });
+
+    // Listen for clicks on the page
+    document.addEventListener('click', function(event) {
+        // If the clicked element is a delete button, handle the delete operation
+        if (event.target.closest('.deleteButton')) {
+            // ... [This part remains unchanged]
+        }
+    });
+
+    // Fetch contacts when the page first loads
+    fetchContacts();
+});
 </script>
+
+
 
 
 
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Starfinder</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Include external CSS stylesheet -->
-    <link rel="stylesheet" href="styles.css">
-    <!-- Link to favicon for the website -->
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>Starfinder</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Include external CSS stylesheet -->
+        <link rel="stylesheet" href="styles.css">
+        <!-- Link to favicon for the website -->
+        <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    </head>
 
-<body>
-    <!-- Background video -->
-    <video autoplay muted loop plays-inline class="background-video">
-        <source src="images/nebula.mp4" type="video/mp4">
-    </video>
+    <body>
+        <!-- Search Form -->
+        <form id="searchForm" style="left:300px; top: 5px;">
+        <input type="text" name="search" id="searchInput" placeholder="Search by username">
+            <input type="submit" value="Search">
+        </form>
 
-    <div class="main">
-        <!-- Sign out link -->
-        <a href="contacts.php?signout=true" class="glow-on-hover" style="right: 10px; top: 5px;">Sign Out</a>
-        <!-- Contacts title -->
-        <h1 class="floating glowing title">Contacts</h1>
-        <!-- Add new contact link -->
-        <a href="addcontacts.php" style="left:10px; top: 5px;" class="glow-on-hover">Add New Contact</a>
-        <!-- Table for displaying contacts -->
-        <div class="box">
-        <table class="contact-table">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>View</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
+        <!-- Background video -->
+        <video autoplay muted loop plays-inline class="background-video">
+            <source src="images/nebula.mp4" type="video/mp4">
+        </video>
 
-        </table>
+        <div class="main">
+            <!-- Sign out link -->
+            <a href="contacts.php?signout=true" class="glow-on-hover" style="right: 10px; top: 5px;">Sign Out</a>
+            <a href="addcontacts.php" class="glow-on-hover" style="right: 10px; top: 5px;">Add New Contact</a>
+            <!-- Contacts title -->
+            <h1 class="floating glowing title">Contacts</h1>
+            <!-- Add new contact link -->
+            
+            <!-- Table for displaying contacts -->
+            <div class="contact-table box">
+            <table class="contact-table">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>View</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+
+            </table>
+            </div>
         </div>
-    </div>
-</body>
-
+    </body>
 </html>
